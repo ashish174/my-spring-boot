ae830e62e4c20b69caba87bf9ce15f63eb5c72aa
AshishKumar <dhruva.ashu@gmail.com>
2019-06-17 11:55:20 +0530

    Adding Student

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/apps")
 public class HelloController {
-    
-    @RequestMapping("/")
+
+    @RequestMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+    @RequestMapping("/student123")
+    public Student getStudent(){
+        Student s1 = new Student("Ashish", 30);
+        return s1;
+    }
+
+
 }
---
+++ complete/src/main/java/hello/Student.java
@@ -0,0 +1,65 @@
+package hello;
+
+import java.util.Objects;
+
+public class Student {
+  private long id;
+  private String name;
+  private int age;
+  private static int idCount = 1000;
+
+  public Student() {
+  }
+
+  public Student(String name, int age) {
+    this.id = idCount++;
+    this.name = name;
+    this.age = age;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Student student = (Student) o;
+    return id == student.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", age=" + age +
+        '}';
+  }
+}
